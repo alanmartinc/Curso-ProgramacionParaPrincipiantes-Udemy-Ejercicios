361f75f71c1b8d07a6736bc430d9f8ea13ee1b80
alanmartinc <alancabot.ac@gmail.com>
2020-04-30 18:48:46 -0300

    Se agrega tarea 8

---
+++ TAREA 8/app25.js
@@ -0,0 +1,23 @@
+function masLargo(heroes) {
+
+    let nombreLargo = '';
+
+    for(let i = 0; i < heroes.length; i++) {
+
+        let nombre = heroes[i];
+
+        if (nombre.length > nombreLargo.length) {
+            nombreLargo = nombre;
+        }
+
+    }
+
+    return nombreLargo;
+
+}
+
+let heroes = ['Deadpool', 'Ciclope', 'Magneto', 'Profesor Charles Xavier'];
+let heroeLargo = masLargo(heroes);
+
+
+console.log(heroeLargo); // Profesor Charles Xavier
