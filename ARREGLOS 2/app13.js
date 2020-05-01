00457a0f849fb00f97faf2e613e99203da068a35
alanmartinc <alancabot.ac@gmail.com>
2020-05-01 19:25:19 -0300

    Se agrega ejercicio arreglos 2

---
+++ ARREGLOS 2/app13.js
@@ -0,0 +1,8 @@
+let personas = ['Fernando', 'Francisco', 'Maria'];
+let salarios = [1000, 1500, 3000];
+
+for (let i = 0; i < personas.length; i++) { // length: propiedad que dice cuantos elementos tiene
+
+    console.log(personas[i] + ' Tiene un salario de ' + salarios[i]);
+
+}
