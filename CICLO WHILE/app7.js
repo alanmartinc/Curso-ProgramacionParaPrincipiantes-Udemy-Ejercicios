cccb125ce7b8db297ecfdfe47df236ee615e07ec
alanmartinc <alancabot.ac@gmail.com>
2020-05-01 18:18:42 -0300

    Se agrega ejercicio ciclo while

---
+++ CICLO WHILE/app7.js
@@ -0,0 +1,11 @@
+let gasolina = 100;
+
+while (gasolina > 0) {
+    
+    gasolina--; // gasolina = gasolina -1;
+
+    console.log('Gasolina restante ' + gasolina);
+    
+}
+
+console.log('Ya no tiene gasolina');
